24b0da853e2c104567fe7cab814deb71732ffe5e
phoenixredwolf <b.vogtman@outlook.com>
2023-08-03 17:51:54 -0400

    added form to contact page

---
+++ Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoenixRedwolfDigitalServices.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string Company { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public int Zip { get; set; } = 00000;
+        public string OfficePhone { get; set; } = string.Empty;
+        public string CellPhone { get; set; } = string.Empty;
+        [Required]
+        public string Details { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ Models/State.cs
@@ -0,0 +1,8 @@
+﻿namespace PhoenixRedwolfDigitalServices.Models
+{
+    public class State
+    {
+        public String Name { get; set; }
+        public String Abbr { get; set; }
+    }
+}

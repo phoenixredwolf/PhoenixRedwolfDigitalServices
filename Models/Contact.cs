1d990339ef363a6e617b9d5e0f41ef9c2c2c2c09
phoenixredwolf <b.vogtman@outlook.com>
2023-08-04 15:49:58 -0400

    updated model

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -10,8 +10,14 @@ namespace PhoenixRedwolfDigitalServices.Models
         public string Address { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
         public string State { get; set; } = string.Empty;
-        public int Zip { get; set; } = 00000;
+        [StringLength(5)]
+        public string PostalCode { get; set; } = "00000";
+        [Required]
+        [EmailAddress] 
+        public string Email { get; set; }
+        [Phone]
         public string OfficePhone { get; set; } = string.Empty;
+        [Phone]
         public string CellPhone { get; set; } = string.Empty;
         [Required]
         public string Details { get; set; } = string.Empty;
